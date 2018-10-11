8dc539a6812ab9f35a0f23d404df6e6d33eae2ea
eronne <erwann.letue@gmail.com>
2018-10-11 14:30:30 +0200

    Init LoadObject project

---
+++ src/components/pages/LoadObject/LoadObject.vue
@@ -0,0 +1,93 @@
+<template>
+  <section class="page page--planes">
+  </section>
+</template>
+
+<script>
+import * as THREE from 'three'
+import GLTFLoader from './assets/js/gltfloader'
+
+export default {
+  name: 'load-object',
+  mounted () {
+    this.createScene()
+    this.addListeners()
+  },
+  methods: {
+    addListeners () {
+      window.addEventListener('resize', this.setRendererSize)
+      window.addEventListener('mousemove', this.getMousePosition)
+      window.addEventListener('click', this.scalePlaneWhenIntersect)
+    },
+    removeListeners () {
+      window.removeEventListener('resize', this.setRendererSize)
+      window.removeEventListener('mousemove', this.getMousePosition)
+      window.removeEventListener('click', this.scalePlaneWhenIntersect)
+    },
+    createScene () {
+      this.scene = new THREE.Scene()
+
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+      this.camera.position.z = 300
+
+      this.renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+      })
+      this.setRendererSize()
+      this.$el.appendChild(this.renderer.domElement)
+
+      this.light = new THREE.AmbientLight(0xfffff, 1.3)
+      this.scene.add(this.light)
+
+      this.loadObject()
+
+      this.render()
+    },
+    loadObject () {
+      this.loader = new THREE.GLTFLoader()
+
+      this.loader.load(
+        './../../../../static/gltf/scene.gltf',
+        (gltf) => {
+          this.scene.add( gltf.scene );
+
+          gltf.animations
+          gltf.scene
+          gltf.scenes
+          gltf.cameras
+          gltf.asset
+        },
+        function (xhr) {
+          console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+        },
+        // called when loading has errors
+        function (error) {
+          console.log(error)
+        }
+      )
+    },
+    render () {
+      requestAnimationFrame(this.render)
+
+      this.renderer.render(this.scene, this.camera)
+    },
+    setRendererSize () {
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+      this.renderer.setSize(this.width, this.height)
+      this.camera.aspect = this.width / this.height
+      this.camera.updateProjectionMatrix()
+    }
+  },
+  beforeDestroy () {
+    this.removeListeners()
+  }
+}
+</script>
+
+<style scoped>
+.page--planes {
+  position: relative;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/pages/HomePage/HomePage'
 import PlanesFullScreen from '@/components/pages/PlanesFullScreen/PlanesFullScreen'
+import LoadObject from '@/components/pages/LoadObject/LoadObject'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/planes-full-screen',
       name: 'PlanesFullScreen',
       component: PlanesFullScreen
+    },
+    {
+      path: '/load-object',
+      name: 'LoadObject',
+      component: LoadObject
     }
   ]
 })
